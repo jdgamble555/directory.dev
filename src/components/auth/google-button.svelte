ec7a0df9db99f3c610e54f8c81aaf5f4fe68e07d
Jonathan Gamble <jdgamble555@gmail.com>
2023-02-17 22:15:34 -0600

    google login

--- src/components/auth/google-button.svelte
+++ src/components/auth/google-button.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { signInWithGoogle } from '$lib/supabase/auth';
 	import GoogleIcon from './google-icon.svelte';
 </script>
 
 <button
 	class="bg-transparent hover:bg-black hover:bg-opacity-5  text-blue-700 font-semibold py-2 px-4 rounded border flex"
+	on:click={signInWithGoogle}
 >
 	<GoogleIcon class="h-6 w-6 mr-3" />
 	Sign in with Google
